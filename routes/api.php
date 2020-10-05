ee6233b8f23cd11698a6a95e7ae33ae19120eadd
SaNiKr <sanikr@outlook.com>
2020-10-05 02:32:38 -0600

    Test firebase

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', 'CharacterController@test');
+Route::get('/test', 'ChatController@test');
 
 Route::group([
     'middleware' =>  ['api'],
